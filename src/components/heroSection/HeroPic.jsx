0f56037323461a55bc75e378d1fa180c967f70c1
suyash002 <getontosuyash@gmail.com>
2025-04-21 09:52:44 +0530

    create portfolia on react js and tailwind css

--- src/components/heroSection/HeroPic.jsx
+++ src/components/heroSection/HeroPic.jsx
@@ -12,7 +12,7 @@ const HeroPic = () => {
       className=" h-full flex items-center justify-center "
     >
       <img
-        src="../../public/images/ProfilePicc.png"
+        src="../../../public/images/ProfilePicc.png"
         alt="Suyash Upadhyay"
         className="max-h-[450px] w-[80%] rounded-3xl"
       />
